6747bb4fdc540ca30569de5fbcc3e9be3707abc7
Hyongsop Kim <hyongsop.kim@nvisions.co.kr>
2020-03-18 10:47:56 +0900

    android version is uploaded.

---
+++ mobile/android/app/src/main/java/com/nvisions/solutionsforaccessibility/MainActivity.java
@@ -0,0 +1,33 @@
+package com.nvisions.solutionsforaccessibility;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button liveRegion = (Button) findViewById(R.id.live_region);
+        liveRegion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LiveRegionActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button labelFor = (Button) findViewById(R.id.label_for);
+        labelFor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LabelForActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
