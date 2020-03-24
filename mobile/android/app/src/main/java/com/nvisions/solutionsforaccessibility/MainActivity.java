060a3057490c93db7e16c7c20fe3886a153243b1
Hyongsop Kim <hyongsop.kim@nvisions.co.kr>
2020-03-24 10:57:49 +0900

    add slide demo

--- mobile/android/app/src/main/java/com/nvisions/solutionsforaccessibility/MainActivity.java
+++ mobile/android/app/src/main/java/com/nvisions/solutionsforaccessibility/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
         Button labelFor = (Button) findViewById(R.id.label_for);
         labelFor.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,5 +30,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button carousel = (Button) findViewById(R.id.carousel);
+        carousel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CarouselActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
